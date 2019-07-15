a1c4057a84e756bb75882e296e949c9cd6948448
AlekseiChirkov <tektonikboy98@gmail.com>
2019-07-15 15:43:53 +0600

    Update settings.py

--- djangoTask/settings.py
+++ djangoTask/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangoTask',
         'USER': 'postgres',
-        'PASSWORD': config('DB_PASS')
+        'PASSWORD': config('DB_PASS'),
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
